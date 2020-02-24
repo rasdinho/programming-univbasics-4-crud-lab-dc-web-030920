020b19c6f38e6d1c24e5f0be6021fed9996ffe04
Ras azzab <ras.dinho7@gmail.com>
2020-02-24 01:33:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,33 +1,33 @@
 def create_an_empty_array
-  
+  array=[]
 end
 
 def create_an_array
-  
+  array=["lil","caca","milan","raja"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+ array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop()
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[index=0]
 end
 
 def retrieve_last_element_from_array(array)
@@ -35,5 +35,5 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-
+array[element]
 end
